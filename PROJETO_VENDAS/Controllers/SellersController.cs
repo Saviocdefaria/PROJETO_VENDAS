33070a97af5594cbe4440e7f4b2a4fdb64c2f64a
Sávio Carvalho <saviocarvalho023@gmail.com>
2026-03-11 16:48:54 -0300

    API CRUD COMPLETA

--- PROJETO_VENDAS/Controllers/SellersController.cs
+++ PROJETO_VENDAS/Controllers/SellersController.cs
@@ -23,5 +23,66 @@ namespace PROJETO_VENDAS.Controllers
             var sellers = _context.Sellers.ToList();
             return Ok(sellers);
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Seller> GetSeller(int id)
+        {
+            var seller = _context.Sellers.Find(id);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(seller);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Seller> CreateSeller(Seller seller)
+        {
+            _context.Sellers.Add(seller);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetSeller), new { id = seller.Id }, seller);
+        }
+
+        [HttpPut]
+        public ActionResult<Seller> PutSeller(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            var existingSeller = _context.Sellers.Find(id); 
+            if (existingSeller == null) 
+            {
+                return NotFound();
+            }
+            existingSeller.Name = seller.Name;
+            existingSeller.Email = seller.Email;
+            existingSeller.BirthDate = seller.BirthDate;
+            existingSeller.BaseSalary = seller.BaseSalary;
+            existingSeller.DepartmentId = seller.DepartmentId;
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteSeller(int id) 
+        {
+            var existingSeller = _context.Sellers.Find(id);
+            if ( existingSeller == null)
+            {
+                return NotFound();
+            }
+            _context.Sellers.Remove(existingSeller);
+            _context.SaveChanges();
+            return NoContent();
+
+        }
+
     }
 }
